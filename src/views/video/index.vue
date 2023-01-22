45a033be740fc3cc0f1ad0c5ab9732b3ada2866d
mason369 <1960638223@qq.com>
2023-01-22 15:39:01 +0800

    update:更新路由

---
+++ src/views/video/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+  视频
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped lang="scss">
+
+</style>
--- vue.config.js
+++ vue.config.js
@@ -4,7 +4,8 @@ const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({transpileDependencies: true});
 
 module.exports = {
-	publicPath: "/Heimatoutiao/"
+	publicPath: "/Heimatoutiao/",
+	lintOnSave: false
 	// publicPath: process.env.NODE_ENV === "production" ? "./" : "/",
 
 	// //代理服务器
