fb3fd74503e6f959e653f8e67d9b0114b7538feb
mason369 <1960638223@qq.com>
2023-01-25 20:50:13 +0800

    添加首页文章列表下拉刷新

--- src/views/home/components/article-list.vue
+++ src/views/home/components/article-list.vue
@@ -1,17 +1,24 @@
 <template>
 	<div class="article-cotainer" ref="article-list">
-		<van-list
-			v-model="loading"
-			:finished="finished"
-			finished-text="没有更多了"
-			@load="onLoad"
+		<van-pull-refresh
+			v-model="isRefresh"
+			@refresh="onRefresh"
+			:success-text="refreshSuccessText"
+      :success-duration="1200"
 		>
-			<van-cell
-				v-for="item in articles"
-				:key="item.id"
-				:title="item.title"
-			/>
-		</van-list>
+			<van-list
+				v-model="loading"
+				:finished="finished"
+				finished-text="没有更多了"
+				@load="onLoad"
+			>
+				<van-cell
+					v-for="item in articles"
+					:key="item.id"
+					:title="item.title"
+				/>
+			</van-list>
+		</van-pull-refresh>
 	</div>
 </template>
 
@@ -29,31 +36,58 @@ export default {
 	data() {
 		return {
 			// 列表数据
-			articles : [],
+			articles          : [],
 			// 是否加载中
-			loading  : false,
+			loading           : false,
 			// 是否加载完成
-			finished : false,
+			finished          : false,
 			// 时间戳
-			timestamp: 1556789000001
+			timestamp         : 1556789000001,
+			// 是否下拉刷新
+			isRefresh         : false,
+			// 下拉刷新成功提示
+			refreshSuccessText: ""
 		};
 	},
 	methods: {
+		//当前时间戳
 		async onLoad() {
-			console.log("sd");
-			const { data } = await getArticles({
-				channel_id: this.channels[0].id,
-				timestamp : this.timestamp || Date.now(),
-				with_top  : 1
-			});
-			const { results } = data;
-			this.articles.push(...results);
-			this.loading = false;
-			if (results.length) {
-				this.timestamp = data.pre_timestamp;
+			try {
+				const { data } = await getArticles({
+					channel_id: this.channels[0].id,
+					timestamp : this.timestamp++,
+					with_top  : 1
+				});
+				const { results } = data;
+				this.articles.push(...results);
+				this.loading = false;
+				if (results.length) {
+					this.timestamp = data.pre_timestamp;
+				}
+				else {
+					this.finished = true;
+				}
+				console.log(data);
+				console.log(this.timestamp);
 			}
-			else {
-				this.finished = true;
+			catch (e) {
+				this.$toast.fail(e.message);
+			}
+		},
+		async onRefresh() {
+			try {
+				const { data } = await getArticles({
+					channel_id: this.channels[0].id,
+					timestamp : this.timestamp++,
+					with_top  : 1
+				});
+				this.articles.unshift(...data.results);
+				this.isRefresh = false;
+				console.log(data);
+				this.refreshSuccessText = `更新了${data.results.length}条数据`;
+			}
+			catch (e) {
+				this.$toast.fail(e.message);
 			}
 		}
 	},
