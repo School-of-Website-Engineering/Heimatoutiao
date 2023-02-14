25789c0953cc4f83892fde885b66afea266bf0be
mason369 <1960638223@qq.com>
2023-02-14 17:56:04 +0800

    添加文章详情图片预览

--- src/views/article/index.vue
+++ src/views/article/index.vue
@@ -21,16 +21,21 @@
 				size="small"
 				class="right-btn"
 				:icon="article.is_followed ? '' : 'plus'"
-				>{{ article.is_followed ? "已关注" : "关注" }}</van-button
-			>
+				>{{ article.is_followed ? "已关注" : "关注" }}
+			</van-button>
 		</van-cell>
-		<div class="content markdown-body" v-html="article.content"></div>
+		<div
+			class="content markdown-body"
+			v-html="article.content"
+			ref="article-content"
+		></div>
 	</div>
 </template>
 
 <script>
 import "./github-markdown-light.css";
 import { getArticle } from "@/api";
+import { ImagePreview } from "vant";
 
 export default {
 	name: "Artice",
@@ -42,6 +47,7 @@ export default {
 	},
 	created() {
 		this.loadArticle();
+		console.log("--------------原始content↓------------------");
 	},
 	props: {
 		articleId: {
@@ -55,7 +61,37 @@ export default {
 			console.log("---------------文章数据↓-----------------");
 			console.log(data);
 			this.article = data;
+			//替换头像
 			this.replaceAutPhoto();
+			//替换图片
+			this.replaceContentImg();
+			console.log(this.article.content);
+
+			//图片预览
+			this.$nextTick(() => {
+				this.handlePerviewImage();
+			});
+			//调用预览
+		},
+		handlePerviewImage() {
+			//获取dom
+			const articleContentDom = this.$refs["article-content"];
+			console.log(articleContentDom);
+			//得到img
+			const imgs = articleContentDom.querySelectorAll("img");
+			console.log(imgs);
+			const imgPaths = [];
+			//循环注册事件
+			imgs.forEach((img, index) => {
+				//每循环一次将src添加到数组
+				imgPaths.push(img.src);
+				img.onclick = function() {
+					ImagePreview({
+						images       : imgPaths,
+						startPosition: index
+					});
+				};
+			});
 		},
 		//随机返回一张图片
 		replaceAutPhoto() {
@@ -67,6 +103,22 @@ export default {
 			];
 			let randomIndex = Math.floor(Math.random() * photos.length);
 			this.article.aut_photo = photos[randomIndex];
+		},
+		//如果this.article.content中包含'"//upload-images'字符，则从该字符开始删除，一直删到带有'"'字符时结束，并将删除的字符替换为"https://img01.yzcdn.cn/vant/cat.jpeg"
+		//正则实现,以此类推
+		replaceContentImg() {
+			this.article.content = this.article.content.replace(
+				/\/\/upload-images.*?['"]/g,
+				"https://img01.yzcdn.cn/vant/cat.jpeg"
+			);
+			this.article.content = this.article.content.replace(
+				/src=".*?['"]/g,
+				"src=\"https://img01.yzcdn.cn/vant/cat.jpeg\""
+			);
+			this.article.content = this.article.content.replace(
+				/data-original-src=".*?['"]/g,
+				"src=\"https://img01.yzcdn.cn/vant/cat.jpeg\""
+			);
 		}
 	}
 };
@@ -76,32 +128,39 @@ export default {
 .markdown-body {
 	padding: 14px;
 	background-color: #fff;
+
 	ul {
 		list-style: rean-hanja-informal;
 	}
 }
+
 .user-info {
 	.name {
 		font-size: 12px;
 		color: #333;
 	}
+
 	.time {
 		font-size: 11px;
 		color: #b4b4b4;
 	}
+
 	.user-avatar {
 		width: 35px;
 		height: 35px;
 		margin-right: 8px;
 	}
+
 	.right-btn {
 		width: 85px;
 		height: 29px;
 	}
 }
+
 ::v-deep .van-nav-bar .van-icon {
 	color: #fff;
 }
+
 .title {
 	text-align: center;
 	font-size: 20px;
