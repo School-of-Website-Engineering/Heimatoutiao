ba284fb8bb115b62c1af82bc5cb41bd5b7c22252
mason369 <1960638223@qq.com>
2023-02-14 15:59:17 +0800

    更新文章详情页面布局

--- src/views/article/index.vue
+++ src/views/article/index.vue
@@ -1,5 +1,28 @@
 <template>
-	<div></div>
+	<!--顶部-->
+	<div>
+		<van-nav-bar title="文章详情" left-arrow @click-left="$router.back()" />
+		<h1 class="title">sadfsadffffffffdsafffffffff</h1>
+		<van-cell class="user-info" center>
+			<div class="name" slot="title">adsfasd</div>
+			<van-image
+				slot="icon"
+				round
+				fit="cover"
+				class="user-avatar"
+				src="https://img01.yzcdn.cn/vant/cat.jpeg"
+			/>
+			<div slot="label" class="time">sadfads</div>
+			<van-button
+				type="info"
+				round
+				size="small"
+				class="right-btn"
+				icon="plus"
+				>关注</van-button
+			>
+		</van-cell>
+	</div>
 </template>
 
 <script>
@@ -10,11 +33,42 @@ export default {
 	},
 	props: {
 		articleId: {
-			type    : String,
-			required: true
-		}
-	}
+			type: String,
+			required: true,
+		},
+	},
 };
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.user-info {
+	.name {
+		font-size: 12px;
+		color: #333;
+	}
+	.time {
+		font-size: 11px;
+		color: #b4b4b4;
+	}
+	.user-avatar {
+		width: 35px;
+		height: 35px;
+		margin-right: 8px;
+	}
+	.right-btn {
+		width: 85px;
+		height: 29px;
+	}
+}
+::v-deep .van-nav-bar .van-icon {
+	color: #fff;
+}
+.title {
+	text-align: center;
+	font-size: 20px;
+	color: #333;
+	padding: 14px;
+	background-color: #fff;
+	margin: 0;
+}
+</style>
