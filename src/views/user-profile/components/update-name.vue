1f573fa5988d370a4cf41ff14fcb72787b07bec6
mason369 <1960638223@qq.com>
2023-02-16 17:32:59 +0800

    更新编辑昵称

---
+++ src/views/user-profile/components/update-name.vue
@@ -0,0 +1,38 @@
+<template>
+	<div>
+		<!-- 导航栏 -->
+		<van-nav-bar
+			class="nav-bar-vant"
+			title="修改昵称"
+			left-text="取消"
+			right-text="完成"
+			@click-left="$emit('close')"
+			@click-right="onClickRight"
+		/>
+		<!--  输入框  -->
+		<van-field
+			v-model="message"
+			rows="2"
+			type="textarea"
+			maxlength="50"
+			placeholder="请输入留言"
+			show-word-limit
+		/>
+	</div>
+</template>
+
+<script>
+export default {
+	name: "UpdateName",
+	data() {
+		return {message: ""};
+	},
+	methods: {onClickRight() {}}
+};
+</script>
+
+<style lang="scss" scoped>
+::v-deep .van-nav-bar__text {
+	color: #fff;
+}
+</style>
--- src/views/user-profile/index.vue
+++ src/views/user-profile/index.vue
@@ -11,26 +11,42 @@
 				height="30"
 			/>
 		</van-cell>
-		<van-cell title="昵称" is-link :value="userProfile.name"></van-cell>
+		<van-cell
+			title="昵称"
+			is-link
+			@click="showEditNameShow = true"
+			:value="userProfile.name"
+		></van-cell>
 		<van-cell
 			title="性别"
 			is-link
 			:value="userProfile.gender === 0 ? '男' : '女'"
 		></van-cell>
 		<van-cell title="生日" is-link :value="userProfile.birthday"></van-cell>
+		<!-- 修改昵称弹出层 -->
+		<van-popup
+			v-model="showEditNameShow"
+			position="bottom"
+			:style="{ height: '100%' }"
+			><update-name @close="showEditNameShow = false"></update-name
+		></van-popup>
 	</div>
 </template>
 
 <script>
+import UpdateName from "./components/update-name.vue";
 import { getUserProfile } from "@/api";
 export default {
 	name: "UserProfile",
 	data() {
 		return {
 			// 个人信息
-			userProfile: {},
+			userProfile     : {},
+			// 修改昵称弹出层
+			showEditNameShow: false
 		};
 	},
+	components: { UpdateName },
 	created() {
 		this.loadUserProfile();
 	},
@@ -39,13 +55,16 @@ export default {
 			const { data } = await getUserProfile();
 			this.userProfile = data;
 			console.log(data);
-		},
-	},
+		}
+	}
 };
 </script>
 
 <style lang="scss" scoped>
 ::v-deep .van-nav-bar .van-icon {
 	color: #fff;
+}
+::v-deep .van-popup {
+	background-color: #f5f7f9;
 }
 </style>
