b0806569a3c05bf04633d647b1ca818a6dfc4d51
mason369 <1960638223@qq.com>
2023-02-16 16:43:37 +0800

    添加个人资料页面布局

--- src/views/user-profile/index.vue
+++ src/views/user-profile/index.vue
@@ -1,5 +1,24 @@
 <template>
-	<div class="user-profile"></div>
+	<div class="user-profile">
+		<!-- 导航栏 -->
+		<van-nav-bar
+			title="个人信息"
+			left-arrow
+			@click-left="onClickLeft($router.back())"
+		/>
+		<van-cell title="头像" center is-link>
+			<van-image
+				src="https://img.yzcdn.cn/vant/cat.jpeg"
+                round
+                fit="cover"
+				width="30"
+				height="30"
+			/>
+		</van-cell>
+		<van-cell title="昵称" is-link value="hello"></van-cell>
+		<van-cell title="性别" is-link value="男"></van-cell>
+		<van-cell title="生日" is-link value="2023-02-16"></van-cell>
+	</div>
 </template>
 
 <script>
@@ -9,8 +28,12 @@ export default {
 		return {};
 	},
 	methods: {},
-	mounted() {}
+	mounted() {},
 };
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+::v-deep .van-nav-bar .van-icon {
+	color: #fff;
+}
+</style>
