cec03d8b61c653a455a22f28589094374aabb6f5
mason369 <1960638223@qq.com>
2023-02-16 16:36:40 +0800

    添加编辑资料跳转

---
+++ src/views/user-profile/index.vue
@@ -0,0 +1,16 @@
+<template>
+	<div class="user-profile"></div>
+</template>
+
+<script>
+export default {
+	name: "UserProfile",
+	data() {
+		return {};
+	},
+	methods: {},
+	mounted() {}
+};
+</script>
+
+<style lang="scss" scoped></style>
